62fd0381a0eddf95829c130615155ebc10068568
imagaf <oswaldoj.tipan@gmail.com>
2025-07-22 14:33:59 -0500

    added computers

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');  
-app.use(cors());               
+const corsOptions = {
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(cors(corsOptions)); 
 
 connectDB();
 app.use(express.json());
